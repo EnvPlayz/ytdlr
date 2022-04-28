ba56cdf064b181c6a1616b82412c1c6cd6108df0
EnvPlayz <82762496+EnvPlayz@users.noreply.github.com>
2022-04-28 05:00:37 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express")
 const cors = require('cors')
 const ytdl = require('ytdl-core')
+const axios = require('axios')
 const app = express()
-
 app.use(cors())
-
 app.get("/",(req,res)=>{
     res.status(404)
     res.end()
@@ -30,6 +29,28 @@ app.get("/download",(req,res)=>{
     ytdl(url,{
         format: type
     }).pipe(res)
+})
+app.get("/instadownload", (req,res) => {
+    var instaVid = req.query.vid
+    const options = {
+        method: 'GET',
+        url: 'https://instagram-media-downloader.p.rapidapi.com/rapid/post.php',
+        params: { url: instaVid },
+        headers: {'X-RapidAPI-Host': 'instagram-media-downloader.p.rapidapi.com',
+        'X-RapidAPI-Key': 'bf7e63e44cmsh29d67ebc33fa19bp11cc9bjsnf365962e6d05'}
+    };
+    axios.request(options).then(function (response) {
+        var vid = response.data.video
+        console.log(vid)
+        axios.get(vid).then(RESPON => {
+            console.log(RESPON.request.sws);
+            res.json(RESPON.request)
+            // res.redirect(vid)
+            // console.log(RESPON._currentUrl)
+        })
+    }).catch(function (error) {
+        console.error(error);
+    });
 })
 app.listen(4000, () => {
     console.log("[🎉] API Launched")
